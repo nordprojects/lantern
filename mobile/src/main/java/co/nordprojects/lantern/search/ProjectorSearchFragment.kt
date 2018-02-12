fe496e155001faf98afb6fdba6159891979b8757
Michael Colville <mike.colville@icloud.com>
2018-02-12 13:53:14 +0000

    Add projector search animation

--- mobile/src/main/java/co/nordprojects/lantern/search/ProjectorSearchFragment.kt
+++ mobile/src/main/java/co/nordprojects/lantern/search/ProjectorSearchFragment.kt
@@ -1,13 +1,17 @@
 package co.nordprojects.lantern.search
 
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import co.nordprojects.lantern.R
-
+import kotlinx.android.synthetic.main.fragment_projector_search.*
 
 class ProjectorSearchFragment : Fragment() {
 
@@ -17,6 +21,26 @@ class ProjectorSearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_projector_search, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startSearchAnimation()
+    }
 
-
+    private fun startSearchAnimation() {
+        lampHead.rotation = 45F
+        val animations = listOf(0F, -45F, -135F, -180F, -270F, -315F).map {
+            ObjectAnimator.ofFloat(lampHead, "rotation", it).apply {
+                duration = 1000
+            }
+        }
+        val animatorSet = AnimatorSet().apply {
+            playSequentially(animations)
+        }
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                animation.start()
+            }
+        })
+        animatorSet.start()
+    }
 }
