ffb3242b918c9b10c743e7a4f131469d2aa37fde
Michael Colville <mike.colville@icloud.com>
2018-02-12 17:34:02 +0000

    Add connecting animation

--- mobile/src/main/java/co/nordprojects/lantern/search/ProjectorConnectingFragment.kt
+++ mobile/src/main/java/co/nordprojects/lantern/search/ProjectorConnectingFragment.kt
@@ -1,12 +1,18 @@
 package co.nordprojects.lantern.search
 
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import co.nordprojects.lantern.R
+import kotlinx.android.synthetic.main.fragment_projector_connecting.*
 
 
 /**
@@ -21,4 +27,49 @@ class ProjectorConnectingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_projector_connecting, container, false)
     }
 
-}// Required empty public constructor
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startConnectingAnimation()
+    }
+
+    private fun startConnectingAnimation() {
+        val halfDuration: Long = 500
+
+        // GROW
+        val grow = ObjectAnimator.ofFloat(rainbow, "scaleX", 4.0f).apply {
+            duration = halfDuration
+            interpolator = LinearInterpolator()
+        }
+        val translate = ObjectAnimator.ofFloat(rainbow, "translationX", 100.0f).apply {
+            duration = halfDuration
+            interpolator = LinearInterpolator()
+        }
+        val growSet = AnimatorSet().apply {
+            playTogether(grow, translate)
+        }
+
+        // SHRINK
+        val shrink = ObjectAnimator.ofFloat(rainbow, "scaleX", 0.0f).apply {
+            duration = halfDuration
+            interpolator = LinearInterpolator()
+        }
+        val translate2 = ObjectAnimator.ofFloat(rainbow, "translationX", 220.0f).apply {
+            duration = halfDuration
+            interpolator = LinearInterpolator()
+        }
+        val shrinkSet = AnimatorSet().apply {
+            playTogether(shrink, translate2)
+        }
+
+        // COMBINED
+        val fullSet = AnimatorSet().apply {
+            playSequentially(growSet, shrinkSet)
+        }
+        fullSet.start()
+        fullSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                animation.start()
+            }
+        })
+    }
+}
