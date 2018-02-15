269975cddd4c3d1968b184b6bbfb1c3cfeaa9c60
Joe Rickerby <joerick@mac.com>
2018-02-15 12:04:41 +0000

    Clean up log messages

--- things/src/main/java/co/nordprojects/lantern/channels/nowplaying/NowPlayingChannel.kt
+++ things/src/main/java/co/nordprojects/lantern/channels/nowplaying/NowPlayingChannel.kt
@@ -131,10 +131,10 @@ class NowPlayingChannel() : Channel() {
 
     val castConnectionListener = object : CastConnection.Listener() {
         override fun onStatusUpdate(appName: String?, status: String?) {
-            Log.i(TAG, "onStatusUpdate $status")
+            Log.i(TAG, "Cast device updated status: $status")
         }
         override fun onMediaStatusUpdate(mediaStatus: CastConnection.MediaStatus) {
-            Log.i(TAG, "mediaStatusUpdate $mediaStatus")
+            Log.i(TAG, "Cast device updated media: $mediaStatus")
 
             this@NowPlayingChannel.mediaStatus = mediaStatus
             mediaStatusUpdateDate = Date()
