f8ca9b4887cf94afbd94ca353b4e5105aee7c82b
Joe Rickerby <joerick@mac.com>
2018-02-20 12:37:03 +0000

    Organisation - move classes to separate files

--- things/src/main/java/co/nordprojects/lantern/comms/ConfigurationServer.kt
+++ things/src/main/java/co/nordprojects/lantern/comms/ConfigurationServer.kt
@@ -4,15 +4,9 @@ import android.content.Context
 import android.os.Handler
 import android.util.Log
 import co.nordprojects.lantern.App
-import co.nordprojects.lantern.ChannelsRegistry
 import co.nordprojects.lantern.shared.ConfigurationConnectionTransport
-import co.nordprojects.lantern.shared.ConfigurationMessage
-import co.nordprojects.lantern.shared.clone
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.*
-import org.json.JSONArray
-import org.json.JSONObject
-import java.util.Observer
 
 /**
  * Server advertises for Nearby connections, and creates ConfigurationConnection objects
@@ -71,72 +65,3 @@ class ConfigurationServer(val context: Context) {
 }
 
 
-class ConfigurationConnection(val transport: ConfigurationConnectionTransport) {
-    private val TAG = ConfigurationConnection::class.java.simpleName
-    private val accelerometerObserver = Observer { _, _ -> accelerometerUpdated() }
-    private val appConfigObserver = Observer { _, _ -> appConfigUpdated() }
-
-    init {
-        transport.onMessageReceived = { m -> onMessageReceived(m) }
-    }
-
-    fun onConnectionAccepted() {
-        Log.i(TAG, "Connected to ${transport.endpointId}")
-        sendStateUpdate()
-        sendAvailableChannels()
-        App.instance.accelerometer.addObserver(accelerometerObserver)
-        App.instance.config.addObserver(appConfigObserver)
-    }
-    fun onMessageReceived(message: ConfigurationMessage) {
-        Log.d(TAG, "Received message from ${transport.endpointId}. $message")
-
-        when (message.type) {
-            ConfigurationMessage.Type.SetPlane -> {
-                App.instance.config.updatePlane(
-                        message.arguments.getString("plane"),
-                        message.body!!
-                )
-            }
-            ConfigurationMessage.Type.Reset -> {
-                App.instance.config.resetToDefaults()
-            }
-            ConfigurationMessage.Type.ListAvailableChannels -> {
-                sendAvailableChannels()
-            }
-            ConfigurationMessage.Type.SetName -> {
-                val newName = message.body!!.getString("name")
-                App.instance.config.updateName(newName)
-            }
-            ConfigurationMessage.Type.Error -> {
-                Log.e(TAG, "Error message received via $this. $message")
-            }
-            else -> { throw IllegalArgumentException("Can't handle message type ${message.type}") }
-        }
-    }
-    fun onDisconnected() {
-        Log.i(TAG, "Disconnected from ${transport.endpointId}")
-        App.instance.accelerometer.deleteObserver(accelerometerObserver)
-        App.instance.config.deleteObserver(appConfigObserver)
-    }
-
-    private fun sendStateUpdate() {
-        val body = App.instance.config.toJson(includingSecrets = false).clone()
-        body.put("direction", App.instance.accelerometer.direction?.jsonName)
-        val message = ConfigurationMessage(ConfigurationMessage.Type.StateUpdate, body = body)
-        transport.sendMessage(message)
-    }
-
-    private fun sendAvailableChannels() {
-        val body = JSONObject()
-        body.put("channels", JSONArray(ChannelsRegistry.channelsInfo.map { it.toJson() }))
-        val message = ConfigurationMessage(ConfigurationMessage.Type.AvailableChannels, body = body)
-        transport.sendMessage(message)
-    }
-
-    private fun accelerometerUpdated() {
-        sendStateUpdate()
-    }
-    private fun appConfigUpdated() {
-        sendStateUpdate()
-    }
-}
