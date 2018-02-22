f387a3861a0fe19aa66d2f3c86102f7ef1361075
Michael Colville <mike.colville@icloud.com>
2018-02-22 14:52:57 +0000

    Handle disconnect in channel config

--- mobile/src/main/java/com/example/androidthings/lantern/channels/config/ChannelConfigActivity.kt
+++ mobile/src/main/java/com/example/androidthings/lantern/channels/config/ChannelConfigActivity.kt
@@ -3,13 +3,19 @@ package com.example.androidthings.lantern.channels.config
 import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import com.example.androidthings.lantern.App
+import com.example.androidthings.lantern.configuration.ConnectionState
 import com.example.androidthings.lantern.shared.ChannelConfiguration
+import java.util.Observer
 
 /**
  * Created by Michael Colville on 12/02/2018.
  */
 
 open class ChannelConfigActivity : AppCompatActivity() {
+
+    private val connectionObserver = Observer { _, _ -> checkConnectionStatus() }
+
     val config: ChannelConfiguration by lazy {
         intent.getParcelableExtra<ChannelConfiguration>(ARG_CONFIG)
     }
@@ -24,5 +30,22 @@ open class ChannelConfigActivity : AppCompatActivity() {
         data.putExtra(ChannelConfigActivity.ARG_CONFIG, config)
         setResult(RESULT_CONFIG_SET, data)
         finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        App.instance.client.addObserver(connectionObserver)
+        checkConnectionStatus()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        App.instance.client.deleteObserver(connectionObserver)
+    }
+
+    private fun checkConnectionStatus() {
+        if (App.instance.client.connectionState == ConnectionState.DISCONNECTED) {
+            finish()
+        }
     }
 }
