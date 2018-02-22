144d9ad6d5d6df8a37f94d89e8e8696d9a0fb0f6
Michael Colville <mike.colville@icloud.com>
2018-02-22 14:43:21 +0000

    Handle disconnect in settings

--- mobile/src/main/java/com/example/androidthings/lantern/settings/SettingsActivity.kt
+++ mobile/src/main/java/com/example/androidthings/lantern/settings/SettingsActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidthings.lantern.settings
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidthings.lantern.App
 import com.example.androidthings.lantern.R
+import com.example.androidthings.lantern.configuration.ConnectionState
 import kotlinx.android.synthetic.main.activity_settings.*
+import java.util.Observer
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val connectionObserver = Observer { _, _ -> checkConnectionStatus() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,22 @@ class SettingsActivity : AppCompatActivity() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.fragment_container, settingsFragment)
         fragmentTransaction.commit()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        App.instance.client.addObserver(connectionObserver)
+        checkConnectionStatus()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        App.instance.client.deleteObserver(connectionObserver)
+    }
+
+    private fun checkConnectionStatus() {
+        if (App.instance.client.connectionState == ConnectionState.DISCONNECTED) {
+            finish()
+        }
     }
 }
