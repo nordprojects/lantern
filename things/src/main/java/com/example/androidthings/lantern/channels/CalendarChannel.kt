580e89be075921e627576037e09ee330938efcf4
Joe Rickerby <joerick@mac.com>
2018-02-28 13:02:06 +0000

    Styling of Calendar channel, using TextViews and animating overlaps

--- things/src/main/java/com/example/androidthings/lantern/channels/CalendarChannel.kt
+++ things/src/main/java/com/example/androidthings/lantern/channels/CalendarChannel.kt
@@ -1,16 +1,24 @@
 package com.example.androidthings.lantern.channels
 
-import android.graphics.Canvas
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.Rect
 import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.RelativeLayout
+import android.widget.TextView
 import biweekly.Biweekly
 import biweekly.component.VEvent
 import com.example.androidthings.lantern.Channel
@@ -23,6 +31,7 @@ import kotlin.concurrent.fixedRateTimer
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.properties.Delegates
 
 /**
  * Shows a day's appointments around a real-world clock.
@@ -31,9 +40,18 @@ import kotlin.math.sin
  *   - "url"
  *       The URL to a public calendar in the iCal format.
  */
+@SuppressLint("RtlHardcoded")
 class CalendarChannel : Channel() {
-    private val TAG = this::class.java.simpleName
-    private var events = listOf<VEvent>()
+    private var events by Delegates.observable(listOf<Event>()) {
+        _, old, new ->
+        if (old != new) {
+            update()
+        }
+    }
+    private val textViews = mutableListOf<TextView>()
+    private val notchViews = mutableListOf<View>()
+    private var visibilityAnimations = listOf<OverlappingVisibilityAnimation>()
+
     private var refreshEventsTimer: Timer? = null
     private val iCalURL: URL? by lazy {
         val urlString = config.secrets?.opt("url") as? String
@@ -42,69 +60,8 @@ class CalendarChannel : Channel() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return object : View(context) {
-            val whitePaint = Paint().apply {
-                color = Color.WHITE
-                strokeWidth = 4.0f
-                typeface = context.resources.getFont(R.font.pt_sans)
-                textSize = 20.0f
-            }
-            val leftAlignPaint = Paint(whitePaint).apply {
-                textAlign = Paint.Align.LEFT
-            }
-            val centerAlignPaint = Paint(whitePaint).apply {
-                textAlign = Paint.Align.CENTER
-            }
-            val rightAlignPaint = Paint(whitePaint).apply {
-                textAlign = Paint.Align.RIGHT
-            }
-
-            override fun onDraw(canvas: Canvas) {
-                super.onDraw(canvas)
-
-                val lineStartRadius = canvas.height/2.0f * 0.7f
-                val lineEndRadius = canvas.height/2.0f * 0.8f
-                val textRadius = canvas.height/2.0f * 0.85f
-
-                canvas.drawColor(Color.BLACK)
-                val calendar = Calendar.getInstance()
-
-                for (event in events) {
-                    val startDate = event.dateStart.value
-                    calendar.time = startDate
-                    val hour = calendar.get(Calendar.HOUR_OF_DAY)
-                    val minute = calendar.get(Calendar.MINUTE)
-
-                    val angle = (hour + minute/60.0f) * 2.0f*PI.toFloat() / 12.0f
-
-                    val centerX = canvas.width/2.0f
-                    val centerY = canvas.height/2.0f
-
-                    canvas.drawLine(
-                            centerX + lineStartRadius * sin(angle),
-                            centerY - lineStartRadius * cos(angle),
-                            centerX + lineEndRadius * sin(angle),
-                            centerY - lineEndRadius * cos(angle),
-                            whitePaint
-                    )
-
-                    val angleDeg = angle * 180f/PI.toFloat()
-                    val textPaint = when (angleDeg) {
-                        in 0..30 -> centerAlignPaint
-                        in 30..150 -> leftAlignPaint
-                        in 150..210 -> centerAlignPaint
-                        in 210..330 -> rightAlignPaint
-                        else -> centerAlignPaint
-                    }
-
-                    canvas.drawText(
-                            event.summary.value,
-                            centerX + textRadius * sin(angle),
-                            centerY - textRadius * cos(angle),
-                            textPaint
-                    )
-                }
-            }
+        return RelativeLayout(context).apply {
+            setBackgroundColor(Color.BLACK)
         }
     }
 
@@ -122,11 +79,6 @@ class CalendarChannel : Channel() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "resume")
-    }
-
     override fun onStop() {
         refreshEventsTimer?.cancel()
         refreshEventsTimer = null
@@ -139,18 +91,262 @@ class CalendarChannel : Channel() {
     }
 
     private fun update() {
-        Log.d(TAG, "Events are $events")
-        view?.invalidate()
+        Log.d(Companion.TAG, "Events: $events")
+        val viewGroup = view as? RelativeLayout ?: return
+
+        val notchRadius = viewGroup.height/2.0f * 0.75f
+        val textRadius = viewGroup.height/2.0f * 0.78f
+
+        viewGroup.removeAllViews()
+        textViews.clear()
+        notchViews.clear()
+
+        // 1: create and position the text views
+        for (event in events) {
+
+            val textView = TextView(context).apply {
+                typeface = resources.getFont(R.font.pt_sans)
+                textSize = 16f
+                setTextColor(Color.WHITE)
+                maxLines = 2
+                text = event.name
+                gravity = gravityForAngle(event.angleDeg)
+            }
+            val notchView = View(context).apply {
+                setBackgroundColor(Color.WHITE)
+            }
+            viewGroup.addView(textView, RelativeLayout.LayoutParams(Companion.TEXT_VIEW_WIDTH, Companion.TEXT_VIEW_HEIGHT).apply {
+                addRule(RelativeLayout.CENTER_IN_PARENT)
+            })
+            viewGroup.addView(notchView, RelativeLayout.LayoutParams(2, 22).apply {
+                addRule(RelativeLayout.CENTER_IN_PARENT)
+            })
+
+            val widthOffset = when(horizontalGravityForAngle(event.angleDeg)) {
+                Gravity.LEFT -> 100
+                Gravity.RIGHT -> -100
+                else -> 0
+            }
+            val heightOffset = when(verticalGravityForAngle(event.angleDeg)) {
+                Gravity.TOP -> 35
+                Gravity.BOTTOM -> -35
+                else -> 0
+            }
+
+            textView.translationX = textRadius * sin(event.angle) + widthOffset
+            textView.translationY = textRadius * -cos(event.angle) + heightOffset
+            notchView.translationX = notchRadius * sin(event.angle)
+            notchView.translationY = notchRadius * -cos(event.angle)
+            notchView.rotation = event.angleDeg
+
+            textViews.add(textView)
+            notchViews.add(notchView)
+        }
+
+        // 2: in the case where events overlap each other, separate them into overlapping groups
+        val ungroupedTextViews = textViews.toMutableList()
+        val overlappingGroups = mutableListOf<Set<TextView>>()
+
+        // make sure the views know their sizes
+        textViews.forEach { it.measure(TEXT_VIEW_WIDTH, TEXT_VIEW_HEIGHT) }
+
+        while (ungroupedTextViews.count() > 0) {
+            val group = mutableSetOf(ungroupedTextViews.first())
+            for (textView in ungroupedTextViews) {
+                if (group.any { viewsIntersect(it, textView) }) {
+                    // this text view intersects with one of the views in this group, so make it
+                    // part of the group.
+                    group.add(textView)
+                }
+            }
+            ungroupedTextViews.removeAll(group)
+            overlappingGroups.add(group)
+        }
+
+        // groups with just one view can be ignored, no animations necessary
+        overlappingGroups.removeAll { it.count() < 2 }
+
+        visibilityAnimations.forEach { it.stop() }
+        visibilityAnimations = overlappingGroups.map { OverlappingVisibilityAnimation(it) }
+        visibilityAnimations.forEach { it.start() }
+    }
+
+    private fun gravityForAngle(deg: Float): Int {
+        val horizontal = horizontalGravityForAngle(deg)
+        val vertical = verticalGravityForAngle(deg)
+        return (horizontal or vertical)
+    }
+
+    private fun verticalGravityForAngle(deg: Float): Int {
+        return when (deg) {
+            in 0..60 -> Gravity.BOTTOM
+            in 60..120 -> Gravity.CENTER_VERTICAL
+            in 120..240 -> Gravity.TOP
+            in 240..300 -> Gravity.CENTER_VERTICAL
+            in 300..360 -> Gravity.BOTTOM
+            else -> Gravity.CENTER_VERTICAL
+        }
+    }
+
+    private fun horizontalGravityForAngle(deg: Float): Int {
+        return when (deg) {
+            in 0..30 -> Gravity.CENTER_HORIZONTAL
+            in 30..150 -> Gravity.LEFT
+            in 150..210 -> Gravity.CENTER_HORIZONTAL
+            in 210..330 -> Gravity.RIGHT
+            else -> Gravity.CENTER_HORIZONTAL
+        }
+    }
+
+    inner class OverlappingVisibilityAnimation(private val views: Collection<View>) {
+        private val visibleSets: List<Set<View>>
+
+        init {
+            // Separate the views into visible sets. 'Visible sets' are sets of views
+            // that don't intersect. For example:
+            // ┌---------┐
+            // │    A ┌--┼------┐
+            // └------┼--┘ B ┌--┼------┐
+            //        └------┼--┘ C ┌--┼------┐
+            //               └------┼--┘ D    │
+            //                      └---------┘
+            // In this example, the sets are [A,C] and [B,D].
+            //
+            // Each view must be in at least one set. The sets are the visible views in
+            // each step of the animation.
+
+            val visibleSets = mutableListOf<Set<View>>()
+            val iterationComplete = {
+                // each text view is in at least one visible set
+                 views.all { view ->
+                     visibleSets.any { it.contains(view) }
+                 }
+            }
+
+            while (!iterationComplete()) {
+                val visibleSet = mutableSetOf<View>()
+                val views = this.views.toMutableList()
+
+                // sort the list such that views not already in a visible set are first, to
+                // give them priority
+                views.sortBy { view -> visibleSets.count { it.contains(view) } }
+
+                // build the set by taking an element from the list one-by-one and adding it
+                // if it doesn't overlap anything already in the set.
+                for (view in views) {
+                    val intersectsAViewInTheSet = visibleSet.any { viewsIntersect(it, view) }
+
+                    if (!intersectsAViewInTheSet) {
+                        visibleSet.add(view)
+                    }
+                }
+
+                visibleSets.add(visibleSet)
+            }
+
+            this.visibleSets = visibleSets
+        }
+
+        private var step = 0
+        private var animator: Animator? = null
+        private val visibleSet get() = visibleSets[step]
+
+        fun start() {
+            stop()
+            views.forEach { textView ->
+                textView.alpha = if (visibleSet.contains(textView)) 1f else 0f
+            }
+            animateToNextStep()
+        }
+
+        fun animateToNextStep() {
+            step += 1
+            if (step >= visibleSets.count()) {
+                step = 0
+            }
+
+            stop()
+            animator = AnimatorSet().apply {
+                startDelay = 5000
+                playTogether(views.flatMap { textView ->
+                    val textViewVisible = visibleSet.contains(textView)
+                    val textViewAlpha = if (textViewVisible) 1f else 0f
+                    val notchView = notchViewForTextView(textView)
+                    val notchViewAlpha = if (textViewVisible) 1f else 0.5f
+                    listOf(
+                            ObjectAnimator.ofFloat(textView, "alpha", textViewAlpha).apply {
+                                duration = 3000
+                                interpolator = AccelerateDecelerateInterpolator()
+                            },
+                            ObjectAnimator.ofFloat(notchView, "alpha", notchViewAlpha).apply {
+                                duration = 3000
+                                interpolator = AccelerateDecelerateInterpolator()
+                            }
+                    )
+                })
+                addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(p0: Animator?) {
+                        animateToNextStep()
+                    }
+                })
+                start()
+            }
+        }
+
+        fun stop() {
+            animator?.removeAllListeners()
+            animator?.end()
+            animator = null
+        }
+    }
+
+    data class Event(val name: String, val startDate: Date) {
+        companion object {
+            val calendar: Calendar = Calendar.getInstance()
+        }
+        val angle: Float
+        val angleDeg: Float
+
+        init {
+            calendar.time = startDate
+            val hour = calendar.get(Calendar.HOUR_OF_DAY) % 12
+            val minute = calendar.get(Calendar.MINUTE)
+            angle = (hour + minute/60.0f) * 2.0f*PI.toFloat() / 12.0f
+            angleDeg = angle * 180f/PI.toFloat()
+        }
+    }
+
+    private fun viewsIntersect(view1: View, view2: View): Boolean {
+        val view1Position = IntArray(2)
+        view1.getLocationOnScreen(view1Position)
+        val view2Position = IntArray(2)
+        view2.getLocationOnScreen(view2Position)
+
+        return Rect.intersects(
+                Rect(
+                        view1Position[0], view1Position[1],
+                        view1Position[0] + TEXT_VIEW_WIDTH, view1Position[1] + TEXT_VIEW_HEIGHT
+                ),
+                Rect(
+                        view2Position[0], view2Position[1],
+                        view2Position[0] + TEXT_VIEW_WIDTH, view2Position[1] + TEXT_VIEW_HEIGHT
+                )
+        )
+    }
+
+    private fun notchViewForTextView(view: View): View? {
+        val index = textViews.indexOf(view)
+        if (index == -1) return null
+
+        return notchViews.getOrNull(index)
     }
 
     inner class RefreshEventsTask : AsyncTask<URL, Void, List<VEvent>?>() {
-        val TAG = this::class.java.simpleName
-
-        override fun doInBackground(vararg params: URL?): List<VEvent>? {
+            override fun doInBackground(vararg params: URL?): List<VEvent>? {
             val url = params.first()!!
 
             val events = downloadEventsFromURL(url) ?: return null
-            return filteredEventsWithin12Hours(events, Date())
+            return filteredEventsWithinA12HourPeriod(events, Date())
         }
 
         override fun onPostExecute(result: List<VEvent>?) {
@@ -158,14 +354,32 @@ class CalendarChannel : Channel() {
                 Log.e(TAG, "null result from RefreshEventsTask")
                 return
             }
-            events = result
-            update()
+            events = result.map { Event(it.summary.value, it.dateStart.value) }
         }
 
-        private fun filteredEventsWithin12Hours(events: List<VEvent>, date: Date): List<VEvent> {
+        private fun filteredEventsWithinA12HourPeriod(events: List<VEvent>, date: Date): List<VEvent> {
             val result = mutableListOf<VEvent>()
-            val startDate = Date(date.time - 6*60*60*1000)
-            val endDate = Date(date.time + 6*60*60*1000)
+            // start the 12 hour period at 8am or 8pm whichever is preceding `date`
+            val calendar = Calendar.getInstance()
+            calendar.time = date
+            when (calendar.get(Calendar.HOUR_OF_DAY)) {
+                in 0..8 -> {
+                    // knock it back to 8pm the previous day
+                    calendar.timeInMillis -= 12*60*60*1000
+                    calendar.set(Calendar.HOUR_OF_DAY, 20)
+                }
+                in 8..20 -> {
+                    calendar.set(Calendar.HOUR_OF_DAY, 8)
+                }
+                else -> {
+                    calendar.set(Calendar.HOUR_OF_DAY, 20)
+                }
+            }
+            calendar.set(Calendar.MINUTE, 0)
+            calendar.set(Calendar.SECOND, 0)
+
+            val startDate = calendar.time
+            val endDate = Date(calendar.timeInMillis + 12*60*60*1000)
 
             for (event in events) {
                 val dateIterator = event.getDateIterator(TimeZone.getDefault())
@@ -204,5 +418,11 @@ class CalendarChannel : Channel() {
                 connection?.disconnect()
             }
         }
+    }
+
+    companion object {
+        private const val TEXT_VIEW_HEIGHT = 53
+        private const val TEXT_VIEW_WIDTH = 150
+        private val TAG = this::class.java.simpleName
     }
 }
