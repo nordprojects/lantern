5a86034c9719b0e67f08fc5eb5ea9c588014f7b1
Joe Rickerby <joerick@mac.com>
2018-03-01 15:40:56 +0000

    Add functionality to reboot the Pi if started in the wrong resolution
    If projector is plugged in after the Pi is powered on, the Pi will
    reboot so the resolution is correct. Resolution negotiation occurs at
    boot, so a reboot is required to change it.

--- things/src/main/java/com/example/androidthings/lantern/LaunchActivity.kt
+++ things/src/main/java/com/example/androidthings/lantern/LaunchActivity.kt
@@ -1,61 +1,65 @@
 package com.example.androidthings.lantern
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
-import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
-import com.google.android.things.AndroidThings
+import com.example.androidthings.lantern.channels.InfoChannel
+import com.google.android.things.device.DeviceManager
 import kotlinx.android.synthetic.main.launch_activity_layout.*
-import java.net.Inet4Address
-import java.net.NetworkInterface
 import java.util.*
 import kotlin.concurrent.fixedRateTimer
-import kotlin.concurrent.timer
 
 /**
  * Created by joerick on 01/03/18.
  */
 class LaunchActivity: Activity() {
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.launch_activity_layout)
-
-        nameTextView.text = "’${App.instance.advertisingName}‘"
-        ipAddressTextView.text = getIpAddresses().joinToString(separator = "\n")
-        androidThingsVersionTextView.text = "Android Things ${AndroidThings.getVersionString()}"
-        lanternVersionTextView.text = with(packageManager.getPackageInfo(packageName, 0)) {
-            "Lantern v$versionName ($versionCode)"
-        }
-
-        Log.d(TAG, "Launch activity created.")
     }
 
     override fun onStart() {
         super.onStart()
 
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }, 10000)
-    }
+        if (isInTheCorrectResolution()) {
+            // load the info channel and hold for 10 seconds
+            fragmentManager.beginTransaction()
+                    .add(R.id.infoChannelViewGroup, InfoChannel())
+                    .commit()
 
-    private fun getIpAddresses(): List<String> {
-        val result = mutableListOf<String>()
-        for (networkInterface in NetworkInterface.getNetworkInterfaces()) {
-            for (address in networkInterface.inetAddresses) {
-                if (address is Inet4Address && !address.isLoopbackAddress) {
-                    result.add(address.hostAddress)
+            Handler().postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }, 10000)
+        } else {
+            // show the resolution error message, countdown and reboot after 10 seconds
+            resolutionErrorViewGroup.visibility = View.VISIBLE
+            val countDownTimer = object : CountDownTimer(10000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    timeRemainingTextView.text = "${millisUntilFinished/1000}"
+                }
+                override fun onFinish() {
+                    timeRemainingTextView.text = "0"
+                    DeviceManager().reboot()
                 }
             }
+            countDownTimer.start()
         }
-        return result
+    }
+
+    private fun isInTheCorrectResolution(): Boolean {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        Log.d(TAG, "Checking screen resolution, height is ${displayMetrics.heightPixels}px")
+
+        return (displayMetrics.heightPixels >= 540)
     }
 
     companion object {
---
+++ things/src/main/java/com/example/androidthings/lantern/channels/InfoChannel.kt
@@ -0,0 +1,49 @@
+package com.example.androidthings.lantern.channels
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidthings.lantern.App
+import com.example.androidthings.lantern.Channel
+import com.example.androidthings.lantern.R
+import com.google.android.things.AndroidThings
+import kotlinx.android.synthetic.main.info_channel.*
+import kotlinx.android.synthetic.main.main_activity_layout.*
+import java.net.Inet4Address
+import java.net.NetworkInterface
+
+/**
+ * Displays information about the projector.
+ *
+ * Created by joerick on 01/03/18.
+ */
+class InfoChannel: Channel() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.info_channel, viewGroup, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nameTextView.text = "’${App.instance.advertisingName}‘"
+        ipAddressTextView.text = getIpAddresses().joinToString(separator = "\n")
+        androidThingsVersionTextView.text = "Android Things ${AndroidThings.getVersionString()}"
+        val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+        lanternVersionTextView.text = with(packageInfo) {
+            "Lantern v$versionName ($versionCode)"
+        }
+    }
+
+    private fun getIpAddresses(): List<String> {
+        val result = mutableListOf<String>()
+        for (networkInterface in NetworkInterface.getNetworkInterfaces()) {
+            for (address in networkInterface.inetAddresses) {
+                if (address is Inet4Address && !address.isLoopbackAddress) {
+                    result.add(address.hostAddress)
+                }
+            }
+        }
+        return result
+    }
+}
