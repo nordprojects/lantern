03decdc516a409fa905ef7288c54bf9c21a2820f
Michael Colville <mike.colville@icloud.com>
2018-03-14 11:14:45 +0000

    Update copy on calendar customise screen

--- mobile/src/main/java/com/example/androidthings/lantern/channels/config/CalendarConfigActivity.kt
+++ mobile/src/main/java/com/example/androidthings/lantern/channels/config/CalendarConfigActivity.kt
@@ -1,6 +1,10 @@
 package com.example.androidthings.lantern.channels.config
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import com.example.androidthings.lantern.R
 import kotlinx.android.synthetic.main.activity_calendar_config.*
 
@@ -14,10 +18,23 @@ class CalendarConfigActivity : ChannelConfigActivity() {
         toolbar.setNavigationIcon(R.drawable.back_chevron)
 
         setChannelButton.setOnClickListener { updateConfig() }
+
+        val text = "Get a public URL in iCal format from your Google Calendar"
+        val settingsLinkText = "settings page"
+        val spannable = SpannableString("$text $settingsLinkText.")
+        spannable.setSpan(UnderlineSpan(), text.length + 1, text.length + 1 + settingsLinkText.length,0)
+        settingsTextView.text = spannable
+
+        settingsTextView.setOnClickListener { onSettingsLinkClicked() }
     }
 
     private fun updateConfig() {
         config.secrets!!.put("url", editText.text)
         finishWithConfigUpdate()
+    }
+
+    private fun onSettingsLinkClicked() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://support.google.com/calendar/answer/37083?hl=en"))
+        startActivity(intent)
     }
 }
