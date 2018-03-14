b305d0692b28c3e89a9c1031e85ce3f5c1305fbb
Joe Rickerby <joerick@mac.com>
2018-03-14 11:18:43 +0000

    Tidy up the channel descriptions

--- things/src/main/java/com/example/androidthings/lantern/ChannelsRegistry.kt
+++ things/src/main/java/com/example/androidthings/lantern/ChannelsRegistry.kt
@@ -17,14 +17,14 @@ object ChannelsRegistry {
             Pair(::CalendarChannel, ChannelInfo(
                     "calendar-clock",
                     "Augmented clock",
-                    "Projects your google calendar events around a real-world clock",
+                    "Projects your Google calendar events around a real-world clock.",
                     Uri.parse("android.resource://com.example.androidthings.lantern/drawable/banner_clock"),
                     customizable = true
             )),
             Pair(::NowPlayingChannel, ChannelInfo(
                     "now-playing",
                     "Now playing",
-                    "Displays song information for whatever's playing through your Cast-enabled speaker",
+                    "Displays song information for whatever's playing through your Cast-enabled speaker.",
                     Uri.parse("android.resource://com.example.androidthings.lantern/drawable/banner_now_playing"),
                     customizable = true
             )),
@@ -45,20 +45,20 @@ object ChannelsRegistry {
             Pair(::LampChannel, ChannelInfo(
                     "lamp",
                     "Spotlight",
-                    "Bring a little drama to your desktop",
+                    "Bring a little drama to your desktop.",
                     Uri.parse("android.resource://com.example.androidthings.lantern/drawable/banner_spotlight")
             )),
             Pair(::WebViewChannel, ChannelInfo(
                     "webview",
                     "Web view",
-                    "Load a URL for your favourite site, a recipe or a fun web app you’re building",
+                    "Load a URL for your favourite site, a recipe or a fun web app you’re building.",
                     Uri.parse("android.resource://com.example.androidthings.lantern/drawable/banner_web_page"),
                     customizable = true
             )),
             Pair(::BlankChannel, ChannelInfo(
                     "blank",
                     "Blank",
-                    "Kill projections for this direction",
+                    "Clear projections for this direction.",
                     Uri.parse("android.resource://com.example.androidthings.lantern/drawable/banner_blank")
             )),
             Pair(::BatSignalChannel, ChannelInfo(
@@ -70,14 +70,14 @@ object ChannelsRegistry {
             Pair(::MessageChannel, ChannelInfo(
                     "message",
                     "Message",
-                    "Display a short message",
+                    "Display a short message.",
                     Uri.parse("android.resource://com.example.androidthings.lantern/drawable/banner_blank"),
                     customizable = true
             )),
             Pair(::InfoChannel, ChannelInfo(
                     "info",
                     "Lantern Info",
-                    "Projects some useful information about the projector",
+                    "Projects some useful information about the projector.",
                     Uri.parse("android.resource://com.example.androidthings.lantern/drawable/banner_info")
             ))
     )
