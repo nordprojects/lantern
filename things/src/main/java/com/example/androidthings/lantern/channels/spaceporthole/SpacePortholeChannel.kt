4f67d894440ece903a081f792bd9172147bb8f2a
Joe Rickerby <joerick@mac.com>
2018-03-20 16:07:41 +0000

    Add North marker to Space Porthole, fading out after 10 seconds

--- things/src/main/java/com/example/androidthings/lantern/channels/spaceporthole/SpacePortholeChannel.kt
+++ things/src/main/java/com/example/androidthings/lantern/channels/spaceporthole/SpacePortholeChannel.kt
@@ -1,11 +1,13 @@
 package com.example.androidthings.lantern.channels.spaceporthole
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.androidthings.lantern.Channel
 import com.example.androidthings.lantern.R
+import kotlinx.android.synthetic.main.space_porthole_channel.*
 import processing.android.PFragment
 
 
@@ -30,18 +32,42 @@ class SpacePortholeChannel : Channel() {
                 .commit()
     }
 
+    override fun onResume() {
+        super.onResume()
+        showNorthMarkerAndFadeOut()
+    }
+
     override fun onHiddenChanged(hidden: Boolean) {
         super.onHiddenChanged(hidden)
 
-        // don't waste cycles rendering in the background
         if (hidden) {
+            // don't waste cycles rendering in the background
             sketch.surface.pauseThread()
         } else {
             sketch.surface.resumeThread()
+            showNorthMarkerAndFadeOut()
         }
 
         // work around a bug where this view appears atop other surface views even when the
         // fragment is hidden
         sketch.surface.surfaceView.visibility = if (hidden) View.INVISIBLE else View.VISIBLE
+    }
+
+    private var northMarkerFadeOutAnimator: ObjectAnimator? = null
+
+    private fun showNorthMarkerAndFadeOut() {
+        if (northMarkerImageView == null) return
+
+        northMarkerFadeOutAnimator?.cancel()
+        northMarkerFadeOutAnimator = null
+
+        northMarkerImageView.alpha = 0.7f
+
+        northMarkerFadeOutAnimator = ObjectAnimator.ofFloat(northMarkerImageView, "alpha", 0.7f, 0f).apply {
+            propertyName = "alpha"
+            duration = 1000
+            startDelay = 9000
+            start()
+        }
     }
 }
